a719d0e17d3c3336fd2dafc30ced344f17939749
Adella <adellakenn@gmail.com>
2019-05-10 02:04:45 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -63,7 +63,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |season_hash|
     # binding.pry
     if season_hash == "age"
-      age = season_hash["age"].to_f
+      age = season_hash["age"]
     end
   end
   binding.pry
