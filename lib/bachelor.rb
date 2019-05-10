3c78fe37eb15495e835e212576b140cf3681893c
Adella <adellakenn@gmail.com>
2019-05-10 02:22:36 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,79 @@
+require 'pry'
+
+# Return the first name of the season's Winner
+#   key = 
+# {"name"=>"Tessa Horst",
+#  "age"=>"26",
+#  "hometown"=>"San Francisco, CA",
+#  "occupation"=>"Social Worker",
+#  "status"=>"Winner"}
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |season_hash|   
+  # binding.pry
+   if season_hash["status"] == "Winner"
+     return season_hash["name"].split(" ")[0]
+    end
+  end
+  # binding.pry
 end
 
+
+# returns the name of the woman who has that occupation
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_hash, info|
+    info.each do |detail|
+      if detail["occupation"] == occupation
+        return detail["name"]
+      end
+    end
+  end
+# binding.pry
 end
 
+
+# Return a counter of the number of contestants who are from that hometown
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season_hash, info|
+    info.each do |detail|
+      if detail["hometown"] == hometown
+        counter += 1 
+      end
+    end
+  end
+  return counter
 end
 
+
+# # Returns the occupation of the first contestant who hails from that hometown
 def get_occupation(data, hometown)
-  # code here
+   data.each do |season_hash, info|
+     info.each do |detail|
+       if detail["hometown"] == hometown
+         return detail["occupation"]
+       end 
+    end 
+  end
 end
+
+#   Return the average age of all of the contestants for that season
+#   Avg == sum/(quantity_of_numbers)
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = 0
+  num_of_ages = 0
+  
+  data[season].each do |season_hash|
+    season_hash.each do |detail, info|
+      if detail == "age"
+        num_of_ages += 1 
+        age += info.to_f
+      end
+    end
+  end
+  return (age/num_of_ages).round
 end
+
+
+
