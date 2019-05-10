7341aec5b41d2124a4d9ddeff51bf332fe80bb1f
Adella <adellakenn@gmail.com>
2019-05-10 00:25:31 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,15 +1,43 @@
+require 'pry'
+
+# Return the first name of the season's Winner
+#   key = 
+# {"name"=>"Tessa Horst",
+#  "age"=>"26",
+#  "hometown"=>"San Francisco, CA",
+#  "occupation"=>"Social Worker",
+#  "status"=>"Winner"}
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |season_hash|   
+  # binding.pry
+   if season_hash["status"] == "Winner"
+     return season_hash["name"].split(" ")[0]
+    end
+  end
+  # binding.pry
 end
 
+
+# returns the name of the woman who has that occupation
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |detail|
+    binding.pry
+    if detail["occupation"] == occupation
+      return info["name"]
+    end
+  end
+binding.pry
 end
 
+
+# Return a counter of the number of contestants who are from that hometown
 def count_contestants_by_hometown(data, hometown)
   # code here
 end
 
+
+#
 def get_occupation(data, hometown)
   # code here
 end
